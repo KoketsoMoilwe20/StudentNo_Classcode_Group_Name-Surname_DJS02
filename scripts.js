332b962175ae2660a5449bd892dfe792aca00e21
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-05-10 06:48:58 +0200

    Check if inputs are numbers

--- scripts.js
+++ scripts.js
@@ -6,4 +6,9 @@ form.addEventListener("submit", (event) => {
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
   result.innerText = dividend / divider;
+
+  if (isNaN(dividend) || isNaN(divider)) { // checking if inputs are numbers.
+    result.classList.add("critical-error") // Changes HTML body to red.
+    result.innerText = "Something critical went wrong. Please reload the page";
+    console.error("Error: Non-numeric input provided");
 });
