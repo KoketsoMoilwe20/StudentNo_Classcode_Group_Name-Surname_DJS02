4d9922a04903460d0c8bc70e941408a81100e513
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-05-10 06:49:57 +0200

    Check if divider is equal to zero

--- scripts.js
+++ scripts.js
@@ -11,4 +11,8 @@ form.addEventListener("submit", (event) => {
     result.classList.add("critical-error") // Changes HTML body to red.
     result.innerText = "Something critical went wrong. Please reload the page";
     console.error("Error: Non-numeric input provided");
+
+  } else if (parseInt(divider) === 0) { // checiking if divider equal 0.
+    result.innerText = "Division not performed. Invalid number provided. Try again";
+    console.error("Error: Division by zero");
 });
